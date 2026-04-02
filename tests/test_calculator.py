560fde7680edbc668d57cb040022e61819cf2f5f
IamL-jh <liamjh25@gmail.com>
2026-04-02 08:43:39 -0500

    Add failing test to demonstrate CI failure

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,5 +4,8 @@ def test_sum() -> None:
     assert sum(2, 3) == 5
     
 def test_resta() -> None:
-    assert resta(5, 3) == 2
+    assert resta(5, 3) == 1
+
+def test_fail() -> None:
+    assert False, "This test is meant to fail"
 
